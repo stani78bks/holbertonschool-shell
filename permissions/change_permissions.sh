7587d4a067909e6682ba1470ac6e9976e76e0618
stani78bks <10648@holbertonstudents.com>
2025-02-12 12:16:31 +0100

    yy

---
+++ permissions/change_permissions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod 640 hello   # Cela définit les permissions en 640 (propriétaire peut lire et écrire, groupe peut lire, autres n'ont aucune permission)
+# ou
+chmod 444 hello   # Cela définit les permissions en 444 (tout le monde peut lire, mais personne ne peut écrire ou exécuter)
